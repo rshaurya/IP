2589c8e2ee3022e832343b52cc7437c289a4795f
rshaurya24 <shaurya.r.pethe@gmail.com>
2023-05-03 13:01:26 +0530

    Changed file path.

--- data_visualisation_pandas/test.py
+++ data_visualisation_pandas/test.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-names = pd.read_csv('/home/shaurya/crypto_dataset/Price-Data/BTC_Bitcoin.csv', usecols=["High", "Low"])
+names = pd.read_csv('crypto_dataset/Price-Data/BTC_Bitcoin.csv', usecols=["High", "Low"])
 print(names)
 
 names.plot(kind='line', ylabel='Price', xlabel='Index', title='Test')
