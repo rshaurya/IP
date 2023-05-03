3bae1be27d4dabf63791b02d23c85097b7f21bcb
rshaurya24 <shaurya.r.pethe@gmail.com>
2023-05-03 12:59:03 +0530

    Testing plots and csv.

---
+++ data_visualisation_pandas/test.py
@@ -0,0 +1,10 @@
+import tkinter
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+names = pd.read_csv('/home/shaurya/crypto_dataset/Price-Data/BTC_Bitcoin.csv', usecols=["High", "Low"])
+print(names)
+
+names.plot(kind='line', ylabel='Price', xlabel='Index', title='Test')
+plt.show()
