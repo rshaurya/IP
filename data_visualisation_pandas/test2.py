c4befc9dfc936498c0b987b2724085bd0bc0bb3d
rshaurya24 <shaurya.r.pethe@gmail.com>
2023-08-28 15:25:08 +0530

    Shifted to new repo. Deletion.

--- data_visualisation_pandas/test2.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-
-df = pd.DataFrame([[1,2,3], [6,7,8], [9,10,11]], columns=['class', 'age', 'grade'], index=['labdhi', 'sha', 'mud'])
-df.drop(index='sha', inplace=True)
-print(df)
